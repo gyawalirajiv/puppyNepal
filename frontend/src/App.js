9224e8b07b9eacbe841dc3cdf9c93cdf2cc497bd
Sushil Subedi <sushil173@gmail.com>
2022-08-08 22:18:48 -0500

    frontend templating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,6 @@ import List from "./views/List";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
